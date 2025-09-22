6aad84a8352ebd1a83f271127c07c2bd1f5f8fa7
Byron Delgado <byrondanipm@gmail.com>
2025-09-22 08:06:49 -0500

    update sql

--- backend/BaseDatos.sql
+++ backend/BaseDatos.sql
@@ -1,41 +1,49 @@
--- Tabla de personas
+IF EXISTS (SELECT name FROM sys.databases WHERE name = N'challenge')
+BEGIN
+    DROP DATABASE challenge;
+END
+GO
+
+CREATE DATABASE challenge;
+GO
+
+USE challenge;
+GO
+
 CREATE TABLE persons (
-	id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,		
-	name VARCHAR(255) NOT NULL,
-	gender VARCHAR(255) NOT NULL,
-	age INT NOT NULL,
-	identification VARCHAR(255) NOT NULL,
-	address VARCHAR(255) NOT NULL,
-	phone VARCHAR(255) NOT NULL
+    person_id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,		
+    name VARCHAR(70) NOT NULL,
+    gender VARCHAR(9) NOT NULL,
+    age INT NOT NULL,
+    identification VARCHAR(10) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    phone VARCHAR(10) NOT NULL
 );
 
--- Tabla de clientes (1:1 con personas)
 CREATE TABLE clients (
-	id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-  personId INT NOT NULL UNIQUE,
-	password VARCHAR(20) NOT NULL,
-	status BIT NOT NULL,
-	FOREIGN KEY (personId) REFERENCES persons(id) ON DELETE CASCADE
+    client_id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    person_id INT NOT NULL UNIQUE,
+    password VARCHAR(20) NOT NULL,
+    status BIT NOT NULL,
+    FOREIGN KEY (person_id) REFERENCES persons(person_id) ON DELETE CASCADE
 );
 
--- Tabla de cuentas (1:N con clientes)
 CREATE TABLE accounts (
-    id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-    numberAccount VARCHAR(255) NOT NULL,
-    accountType VARCHAR(255) NOT NULL,
+    account_id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    client_id INT NOT NULL,
+    number VARCHAR(10) NOT NULL,
+    type VARCHAR(50) NOT NULL,
     balance DECIMAL(19, 4) NOT NULL,
     status BIT NOT NULL,
-    clientId INT NOT NULL,
-	FOREIGN KEY (clientId) REFERENCES clients(id) ON DELETE CASCADE
+    FOREIGN KEY (client_id) REFERENCES clients(client_id) ON DELETE CASCADE
 );
 
--- Tabla de movimientos
 CREATE TABLE movements (
-    id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    movement_id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    account_id INT NOT NULL,
     date DATETIME NOT NULL DEFAULT GETDATE(),
-    movementType VARCHAR(255) NOT NULL,
+    type VARCHAR(7) NOT NULL,
     value DECIMAL(19, 4) NOT NULL,
     balance DECIMAL(19, 4) NOT NULL,
-    accountId INT NOT NULL,
-	FOREIGN KEY (accountId) REFERENCES accounts(id) ON DELETE CASCADE
+    FOREIGN KEY (account_id) REFERENCES accounts(account_id) ON DELETE CASCADE
 );
