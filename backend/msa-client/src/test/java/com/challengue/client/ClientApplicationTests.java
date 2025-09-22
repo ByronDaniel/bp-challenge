b90beb275a458d3d65ca4df9e4dfb938d1308992
Byron Delgado <byrondanipm@gmail.com>
2025-09-22 00:18:08 -0500

    creation of microservices

---
+++ backend/msa-client/src/test/java/com/challengue/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challengue.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
