8db3a73018c2091f17255bd1b09c8373d3ccee22
Byron Delgado <byrondanipm@gmail.com>
2025-09-22 02:16:37 -0500

    input port and client service implementation

---
+++ backend/msa-client/src/main/java/com/challengue/client/application/input/port/ClientInputPort.java
@@ -0,0 +1,18 @@
+package com.challengue.client.application.input.port;
+
+import com.challengue.client.domain.Client;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface ClientInputPort {
+
+  Flux<Client> getAll();
+
+  Mono<Client> getById(Integer id);
+
+  Mono<Client> save(Client client);
+
+  Mono<Client> updateById(Integer id, Client client);
+
+  Mono<Void> deleteById(Integer id);
+}
---
+++ backend/msa-client/src/main/java/com/challengue/client/application/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.challengue.client.application.utils;
+
+public final class Constants {
+
+  public static final String CLIENT_NOT_FOUND = "Client not found";
+  public static final String CLIENT_ALREADY_EXISTS = "Client already exists";
+
+  private Constants() {
+  }
+}
